c104b93424ef4de82d8eb00c91b6d7d3e5fd9888
apmoskevitz <andrew.moskevitz@gdbiw.com>
2024-07-11 16:50:30 -0400

    add project

---
+++ src/ArtifactsPathRepro/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ArtifactsPathRepro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
